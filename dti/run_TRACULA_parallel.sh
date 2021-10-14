2b88d7a14efeb93cbfdd7bac699465637e738135
austinboroshok <44069948+austinboroshok@users.noreply.github.com>
2021-10-14 15:10:40 -0400

    Delete dti directory

--- dti/run_TRACULA_parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-data_dir=/data/jux/mackey_group/austin
-sub_list=$(cat ${data_dir}/TRACULA/scripts/sublist.txt)
-
-for i in ${sub_list}; do
-	sub_id=$(echo ${i})
-	qsub -V -cwd -l h_vmem=7.1G,s_vmem=7G ${data_dir}/TRACULA/scripts/TRACULA.sh ${sub_id}
-done
